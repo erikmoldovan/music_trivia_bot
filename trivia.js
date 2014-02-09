f74beb010b14826cf3f763d1255b22e2be4e95f5
Erik Moldovan <monkofakan@gmail.com>
2014-02-09 16:49:16 -0500

    Abstracted the logic even more. This will require some refactoring to slim down on the number of lines (and the repetition)

--- trivia.js
+++ trivia.js
@@ -7,24 +7,33 @@ function triviaCtrl($scope, $http){
 
 	// Separating out the query building from the app logic
 	buildParams = function(name, userSubmission){
-		var query = {
+		var query,
+			parserMethod;
+
+		if(userSubmission){
+			query = {
 				'name': name,
 				'type': '/music/artist',
 				'/music/artist/album': []
 			};
+			parserMethod = parseAlbums;
+		}else{
+			query = {
+				'name': name,
+				'type': '/music/album',
+				'/music/album/tracks': []
+			};
+			parserMethod = parseSongs;
+		}
 
-		var params = {
-			query: [[query]]
-		};
-
-		accessFreebase(params);
+		accessFreebase({query: JSON.stringify([query])}, parserMethod);
 	};
 
 	// Abstracted MQL query method; stub for refactoring
-	accessFreebase = function(params){
+	accessFreebase = function(params, parserMethod){
 		$http.get(url, {params: params})
 			.success(function(data, status, headers, config){
-				parseAlbums(data);
+				parserMethod(data);
 			}).error(function(data, status, headers, config){
 				console.log(status + ': ERROR');
 			});
@@ -33,11 +42,14 @@ function triviaCtrl($scope, $http){
 	// Abstracted album parsing
 	parseAlbums = function(data){
 		albums = data.result[0]['/music/artist/album'];
-		console.log(albums);
+		
+		for(i = 0; i < albums.length; i++){
+			parseSongs(albums[i]);
+		}
 	};
 
 	// Abstracted song selection logic
 	parseSongs = function(data){
-
+		console.log(data);
 	};
 }
