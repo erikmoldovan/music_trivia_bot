be7e994b1b650bc4d597e6a9698cfe563d4f7fee
Erik Moldovan <monkofakan@gmail.com>
2014-02-09 16:38:26 -0500

    Abstracted out query logic from app logic, since I'm going to have to start doing things with the result next

--- trivia.js
+++ trivia.js
@@ -2,16 +2,26 @@ function triviaCtrl($scope, $http){
 	var url = "https://www.googleapis.com/freebase/v1/mqlread";
 
 	$scope.submitArtist = function(){
-		console.log($scope.artistName);
+		buildParams($scope.artistName, true);
+	};
 
-		var params = {
-			query: [[{
-				'name': $scope.artistName,
+	// Separating out the query building from the app logic
+	buildParams = function(name, userSubmission){
+		var query = {
+				'name': name,
 				'type': '/music/artist',
 				'/music/artist/album': []
-			}]]
+			};
+
+		var params = {
+			query: [[query]]
 		};
 
+		accessFreebase(params);
+	};
+
+	// Abstracted MQL query method; stub for refactoring
+	accessFreebase = function(params){
 		$http.get(url, {params: params})
 			.success(function(data, status, headers, config){
 				parseAlbums(data);
@@ -20,18 +30,14 @@ function triviaCtrl($scope, $http){
 			});
 	};
 
-	// Abstracted MQL query method; stub for refactoring
-	accessFreebase = function(){
-
-	};
-
 	// Abstracted album parsing
 	parseAlbums = function(data){
 		albums = data.result[0]['/music/artist/album'];
+		console.log(albums);
 	};
 
 	// Abstracted song selection logic
 	parseSongs = function(data){
-		
+
 	};
 }
