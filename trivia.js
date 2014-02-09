4a8de02d31c27e9c078d1e6d46d0f2a88f7d3484
Erik Moldovan <monkofakan@gmail.com>
2014-02-09 16:25:17 -0500

    Successfully accessed Google Freebase via MQL. I'm about an hour into coding.

--- trivia.js
+++ trivia.js
@@ -1,5 +1,37 @@
-function triviaCtrl($scope){
+function triviaCtrl($scope, $http){
+	var url = "https://www.googleapis.com/freebase/v1/mqlread";
+
 	$scope.submitArtist = function(){
 		console.log($scope.artistName);
+
+		var params = {
+			query: [[{
+				'name': $scope.artistName,
+				'type': '/music/artist',
+				'/music/artist/album': []
+			}]]
+		};
+
+		$http.get(url, {params: params})
+			.success(function(data, status, headers, config){
+				parseResults(data);
+			}).error(function(data, status, headers, config){
+				console.log(status + ': ERROR');
+			});
+	};
+
+	// Abstracted MQL query method; stub for refactoring
+	accessFreebase = function(){
+
+	};
+
+	// Abstracted album parsing
+	parseAlbums = function(data){
+		console.log(data.result);
+	};
+
+	// Abstracted song selection logic
+	parseSongs = function(data){
+
 	};
 }
