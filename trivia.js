965999e03ca1d033ac2cdd7f0bf8d466d5f97293
Erik Moldovan <monkofakan@gmail.com>
2014-02-09 22:04:17 -0500

    Done. After 4 hours of work, and a half hour of initial scoping/research.
    I'm going to work on beautifying it now with some Bootstrap

--- trivia.js
+++ trivia.js
@@ -1,11 +1,39 @@
 function triviaCtrl($scope, $http){
+
+	// VARIABLE DECLARATION
+
 	var url = "https://www.googleapis.com/freebase/v1/mqlread";
 
 	$scope.songs = [];
+	$scope.answer = {};
+
+	// SCOPE METHODS
 
 	$scope.submitArtist = function(){
+		// Reset songs model
+		$scope.songs = [];
+		$scope.answer = {};
+
 		buildArtistQuery($scope.artistName);
+
+		$('.questions').fadeIn();
 	};
+
+	$scope.checkAnswer = function(){
+		var $notification = $('.notification');
+
+		if(this.song.album_name == $scope.answer.album_name){
+			$notification.text('Correct!')
+				.addClass('green');
+		}else{
+			$notification.text('Wrong answer. It was ' + $scope.answer.album_name)
+				.addClass('red');
+		}
+
+		$notification.fadeIn();
+	}
+
+	// HELPER FUNCTIONS
 
 	// Separating out the query building from the app logic
 	buildArtistQuery = function(name){
@@ -14,7 +42,6 @@ function triviaCtrl($scope, $http){
 			'type': '/music/artist',
 			'/music/artist/album': [{
 				"name": null,
-				"id": null,
 				"releases": [{
 					"track": []
 				}]
@@ -24,7 +51,7 @@ function triviaCtrl($scope, $http){
 		accessFreebase({query: JSON.stringify([query])}, parseAlbums);
 	};
 
-	// Abstracted MQL query method; stub for refactoring
+	// Abstracted MQL query method
 	accessFreebase = function(params, parserMethod){
 		$http.get(url, {params: params})
 			.success(function(data, status, headers, config){
@@ -34,19 +61,36 @@ function triviaCtrl($scope, $http){
 			});
 	};
 
-	// Abstracted album parsing
-	parseAlbums = function(data){
-		albums = data.result[0]['/music/artist/album'];
-		console.log(albums);
-		
-		// for(i = 0; i < albums.length; i++){
-			// console.log(albums[i]);
-			// buildAlbumQuery(albums[i]);
-		// }
+	// Generate random number
+	generateRand = function(array){
+		return Math.floor(Math.random() * array.length);
 	};
 
-	// Abstracted song selection logic
-	parseSongs = function(data){
-		console.log(data.result);
+	// Abstracted album parsing
+	parseAlbums = function(data){
+		var maxItems = 5;
+		var albums = data.result[0]['/music/artist/album'];
+
+		for(i = 0; i < maxItems; i++){
+			var canPass = false;
+
+			do{
+				// Randomize the album selected for fun
+				var album = albums[generateRand(albums)],
+					tracks = album.releases[0].track,
+					track = tracks[generateRand(tracks)];
+
+				// Makes sure that the edge case of an undefined track does not happen
+				// It happens more than you think...
+				if(track != undefined) canPass = true;
+			}while(!canPass)
+
+			$scope.songs.push({
+				"album_name": album.name,
+				"track": track
+			});
+		}
+
+		$scope.answer = $scope.songs[generateRand($scope.songs)];
 	};
 }
